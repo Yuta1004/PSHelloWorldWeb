46dfc280339e6b46b0167a206298135997c5eed0
NakagamiYuta <guguru0014@gmail.com>
2025-01-11 21:40:09 +0900

    [update] tailwindcss の適用方法を更新

--- src/Component/Counter.purs
+++ src/Component/Counter.purs
@@ -24,27 +24,33 @@ init _  = { value: 0 }
 render :: forall w. State -> HH.HTML w Action
 render s =
     HH.div
-        [ HP.class_ $ ClassName "w-screen h-screen flex flex-col justify-center items-center gap-5" ]
+        [ HP.class_ verticalBox ]
         [ HH.div
-            [ (HP.class_ $ ClassName "text-4xl") ]
+            [ HP.class_ titleText ]
             [ HH.text "Counter" ]
         , HH.div
-            [ HP.class_ $ ClassName "flex justify-center items-center gap-10" ]
+            [ HP.class_ horozontalBox ]
             [ HH.button
-                [ HP.class_ $ ClassName "text-blue-700 px-4 py-2 border border-blue-500 rounded"
+                [ HP.class_ primaryButton
                 , HE.onClick \_ -> Increment
                 ]
                 [ HH.text "+" ]
             , HH.div
-                [ HP.class_ $ ClassName "text-xl" ]
+                [ HP.class_ bodyText ]
                 [ HH.text $ show s.value ]
             , HH.button
-                [ HP.class_ $ ClassName "text-blue-700 px-4 py-2 border border-blue-500 rounded"
+                [ HP.class_ primaryButton
                 , HE.onClick \_ -> Decrement
                 ]
                 [ HH.text "-" ]
             ]
         ]
+    where
+        verticalBox = ClassName "w-screen h-screen flex flex-col justify-center items-center gap-5"
+        horozontalBox = ClassName "flex justify-center items-center gap-5"
+        primaryButton = ClassName "text-blue-700 px-4 py-2 border border-blue-500 rounded"
+        titleText = ClassName "text-4xl"
+        bodyText = ClassName "text-xl"
 
 handleAction :: forall o m. Action -> HalogenM State Action () o m Unit
 handleAction = case _ of
